737115375f43587bad472e52528015194ef11c13
Marcos <mp718887@gmail>
2025-12-06 16:57:50 -0300

    aula03: finalizando task list

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
 // App.tsx
 
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { Header } from "./components/header";
 import { TaskList } from "./components/task-list";
-
-// const API_URL = "https://task-list-api-r9wz.onrender.com";
-const API_URL = "http://localhost:8000";
 
 export type Task = {
   id: string;
@@ -13,25 +10,39 @@ export type Task = {
   completed: boolean;
 };
 
+const API_URL = "https://task-list-api-r9wz.onrender.com";
+
 export function App() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
-  const fetchTasks = async () => {
+  const fetchTasks = useCallback(async () => {
     const res = await fetch(`${API_URL}/tasks`);
     return res.json();
-  };
+  }, []);
 
   const addTask = async (taskName: string) => {
-    const newTask = {
-      name: taskName,
-    };
+    const taskPayload = { name: taskName };
 
-    await fetch(`${API_URL}/tasks`, {
+    const res = await fetch(`${API_URL}/tasks`, {
       method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(newTask),
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(taskPayload),
+    });
+
+    const newTask: Task = await res.json();
+    setTasks([...tasks, newTask]);
+  };
+
+  const deleteTask = async (task: Task) => {
+    await fetch(`${API_URL}/tasks/${task.id}`, { method: "DELETE" });
+    setTasks(tasks.filter((t) => t.id != task.id));
+  };
+
+  const updateTaskComplete = async (task: Task) => {
+    const action = task.completed ? "uncomplete" : "complete";
+
+    await fetch(`${API_URL}/tasks/${task.id}/${action}`, {
+      method: "PATCH",
     });
 
     fetchTasks().then((data) => setTasks(data));
@@ -39,12 +50,16 @@ export function App() {
 
   useEffect(() => {
     fetchTasks().then((data) => setTasks(data));
-  }, []);
+  }, [fetchTasks]);
 
   return (
     <div className="bg-gray-100 h-screen">
       <Header addTask={addTask} />
-      <TaskList tasks={tasks} />
+      <TaskList
+        tasks={tasks}
+        deleteTask={deleteTask}
+        updateTaskComplete={updateTaskComplete}
+      />
     </div>
   );
 }
